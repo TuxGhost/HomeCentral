47bffb587371e1c2d28b1c8ed96848624cfc9c5e
Peter Kuda <PETER.KUDA@vdabcampus.be>
2023-11-21 11:51:20 +0100

    Aanpassen van de home page texts

--- Home Central/Controllers/HomeController.cs
+++ Home Central/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Home_Central.Models;
+﻿using Home_Central.Data.Entities;
+using Home_Central.Models;
+using Home_Central.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,15 +10,46 @@ namespace Home_Central.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IHomeService _homeService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IHomeService homeService)
         {
             _logger = logger;
+            _homeService = homeService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
+        {
+            var result = await _homeService.GetHomeTextsAsync();
+            return View(result);
+        }
+        [Authorize]
+        public IActionResult NieuweText()
+        {
+            var value = new HomeText();
+            return View(value);        
+        }
+        [HttpPost]
+        public async Task<IActionResult> SetNieuweText(HomeText value)
+        {
+            try
+            {
+                if(value.Id == 0) 
+                    await _homeService.PostHomeText(value);
+                else
+                    await _homeService.UpdateHomeText(value);
+                var nieuw = new HomeText();
+                return View(nameof(Index));
+            } catch (Exception) 
+            {
+                return Problem();
+            }
+            
+        }
+        [Authorize]
+        public  IActionResult EditText(HomeText homeText)
         {            
-            return View();
+            return View(nameof(NieuweText),homeText);
         }
 
         public IActionResult Privacy()
--- Home Central/Program.cs
+++ Home Central/Program.cs
@@ -1,16 +1,42 @@
 using Home_Central.Data;
+using Home_Central.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")??
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
     throw new InvalidOperationException("Could not connect to database");
+
+var logging = builder.Configuration.GetValue<string>("LoggingEnabled", "false");
+
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseMySQL(connectionString));
 builder.Services.AddDbContext<WoningDbContext>(options =>
-    options.UseMySQL(connectionString));    
+    options.UseMySQL(connectionString));
+// enable logging depending on  value in appsettings.json (not standard)
+if (logging == "true")
+{
+    builder.Services.AddDbContext<HomeDbContext>(options =>
+        options.UseMySQL(connectionString)
+        .LogTo(Console.WriteLine ,
+            new[] { DbLoggerCategory.Database.Command.Name } ,
+            Microsoft.Extensions.Logging.LogLevel.Information,
+            Microsoft.EntityFrameworkCore.Diagnostics.DbContextLoggerOptions.Level | 
+            Microsoft.EntityFrameworkCore.Diagnostics.DbContextLoggerOptions.LocalTime)
+        .EnableSensitiveDataLogging()
+    );
+} else
+{
+    builder.Services.AddDbContext<HomeDbContext>(options =>
+        options.UseMySQL(connectionString)
+    );
+}
+
+builder.Services.AddTransient<IHomeService, HomeService>();
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
