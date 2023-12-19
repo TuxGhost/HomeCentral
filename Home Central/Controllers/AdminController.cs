46ce0270019973463273241d0ae9351184f7b2bd
Peter Kuda <petar@outlook.be>
2023-12-19 09:09:00 +0100

    admin control - lijst gebruikers

---
+++ Home Central/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using Home_Central.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Home_Central.Controllers;
+
+[Authorize]
+public class AdminController : Controller
+{
+    private readonly ApplicationDbContext context;
+    public AdminController(ApplicationDbContext context)
+    {
+        this.context = context;
+    }
+    public async Task<IActionResult> Index()
+    {
+        var users = await context.Users.ToListAsync();        
+        return View(users);
+    }
+    public IActionResult Details()
+    {
+        return View();
+    }
+}
