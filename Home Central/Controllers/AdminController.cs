834d8a3fda6638245db06b989753288d4b526f16
Peter Kuda <petar@outlook.be>
2023-12-19 13:58:06 +0100

    uitbreiding admin page

--- Home Central/Controllers/AdminController.cs
+++ Home Central/Controllers/AdminController.cs
@@ -1,6 +1,5 @@
 ﻿using Home_Central.Data;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -19,6 +18,21 @@ public class AdminController : Controller
     {
         var users = await context.Users.ToListAsync();        
         return View(users);
+    }
+    public async Task<IActionResult> Users()
+    {
+        var users = await context.Users.ToListAsync();
+        return View(users);
+    }
+    public async Task <IActionResult> Roles()
+    {
+        var roles = await context.Roles.ToListAsync();
+        return View(roles);
+    }
+    public async Task<IActionResult> UserRoles()
+    {
+        var userroles = await context.UserRoles.ToListAsync();
+        return View(userroles);
     }
     public IActionResult Details()
     {
