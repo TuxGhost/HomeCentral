149bf29511bae7e95d3dbd789d58e801d23ad776
Peter Kuda <petar@outlook.be>
2023-12-19 13:58:06 +0100

    uitbreiding admin page

---
+++ Home Central/Data/Migrations/20231219122442_admin role seeding.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Home_Central.Data.Migrations
+{
+    public partial class adminroleseeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "0cadf322-9281-4f3b-aa8a-88d16dd7ff86", "4a941d24-579a-4364-a68f-8456269f56f4", "Administrator", "ADMINISTRATOR" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "fc6b6de0-abd9-493c-9fc0-cf728261ab09",
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "cb8319b8-0167-46c0-9f15-0206280833c7", "4c19d282-edd8-44d4-a187-85cd21be57c9" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "0cadf322-9281-4f3b-aa8a-88d16dd7ff86", "fc6b6de0-abd9-493c-9fc0-cf728261ab09" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "0cadf322-9281-4f3b-aa8a-88d16dd7ff86", "fc6b6de0-abd9-493c-9fc0-cf728261ab09" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "0cadf322-9281-4f3b-aa8a-88d16dd7ff86");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "fc6b6de0-abd9-493c-9fc0-cf728261ab09",
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "7fbeb77c-2e15-40e2-a592-372f205bf286", "8518e6e2-6e79-40ca-9846-7f789b410932" });
+        }
+    }
+}
---
+++ Home Central/Data/Seeding/AspNetRolesSeeding.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+namespace Home_Central.Data.Seeding;
+
+public class AspNetRolesSeeding : IEntityTypeConfiguration<IdentityRole>
+{   
+    public void Configure(EntityTypeBuilder<IdentityRole> builder)
+    {
+        builder.HasData(
+            new IdentityRole
+            {
+                Id = "0cadf322-9281-4f3b-aa8a-88d16dd7ff86",
+                Name = "Administrator",
+                NormalizedName = "ADMINISTRATOR",
+            }
+            );
+    }
+}
